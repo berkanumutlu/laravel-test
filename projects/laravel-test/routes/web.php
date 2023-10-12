131d7b4c4072927180daf99b377153f084ddce10
Berkan Ümütlü <berkanxtr@gmail.com>
2023-10-12 18:53:29 +0300

    Route name/is/current route #2

--- projects/laravel-test/routes/web.php
+++ projects/laravel-test/routes/web.php
@@ -22,5 +22,7 @@ use Illuminate\Support\Facades\Route;
  */
 //Route::get('/', "HomeController@index");
 //Route::get('/', function () { return view('web.home.index'); })->name('home');
-Route::get('/', [\App\Http\Controllers\HomeController::class, "index"])->name('home');
-Route::get('/about', [\App\Http\Controllers\AboutController::class, "index"])->name('about');
+Route::get('/', [\App\Http\Controllers\HomeController::class, "index"])
+     ->name('home');
+Route::get('/about', [\App\Http\Controllers\AboutController::class, "index"])
+     ->name('about');
