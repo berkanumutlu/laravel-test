fddfbb9ee3736cdc8c7e58819d9063bf4999b764
Berkan Ümütlü <berkanxtr@gmail.com>
2023-10-23 15:36:15 +0300

    Controller'dan View'a veri gönderme

--- projects/laravel-test/app/Http/Controllers/HomeController.php
+++ projects/laravel-test/app/Http/Controllers/HomeController.php
@@ -5,6 +5,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('web.home.index');
+        $name = 'berkan';
+        $age = 25;
+        $person = new \stdClass();
+        $person->name = $name;
+        $person->age = $age;
+        //return view('web.home.index', ['name_view' => $name]);
+        //return view('web.home.index', compact('name'));
+        //return view('web.home.index')->with('name', $name)->with('age', $age);
+        //return view('web.home.index')->with(['name' => $name, 'age' => $age]);
+        return view('web.home.index', compact(['person', 'name', 'age']));
     }
 }
--- projects/laravel-test/resources/views/web/home/index.blade.php
+++ projects/laravel-test/resources/views/web/home/index.blade.php
@@ -4,6 +4,10 @@
 @endsection
 @section("content")
     <p>Home Page Content</p>
+    <p>Name: {{ $name ?? 'No Data' }}</p>
+    <p>Age: {{ $age ?? 'No Data' }}</p>
+    <p>Name: {{ $person->name ?? 'No Data' }}</p>
+    <p>Age: {{ $person->age ?? 'No Data' }}</p>
 @endsection
 @section("scripts")
 
