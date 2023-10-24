d2abbed097a83ca0259b896052adb199e7779121
Berkan Ümütlü <berkanxtr@gmail.com>
2023-10-24 13:52:13 +0300

    Directive If/Switch

--- projects/laravel-test/resources/views/web/home/index.blade.php
+++ projects/laravel-test/resources/views/web/home/index.blade.php
@@ -8,8 +8,28 @@
     <p>Global data person: {{ $persona->name ?? 'No Data' }}</p>
     <p>Name: {{ $name ?? 'No Data' }}</p>
     <p>Age: {{ $age ?? 'No Data' }}</p>
-    <p>Name: {{ $person->name ?? 'No Data' }}</p>
-    <p>Age: {{ $person->age ?? 'No Data' }}</p>
+    @if(isset($person))
+        @if(isset($person->name))
+            <p>Name: {{ $person->name }}</p>
+        @else
+            <p>Name: No Data</p>
+        @endif
+        @if(isset($person->age))
+            <p>Age:
+                @switch($person->age)
+                    @case(10)
+                        Kid
+                        @break
+                    @case(25)
+                        Young
+                        @break
+                    @case(35)
+                        Adult
+                        @break
+                @endswitch
+            </p>
+        @endif
+    @endif
 @endsection
 @section("scripts")
 
